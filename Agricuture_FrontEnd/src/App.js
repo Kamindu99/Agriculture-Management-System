5dce6bb6646662d61630a284abb99b6d518722e6
Kamindu99 <kamidugayantha123@gmail.com>
2021-10-02 13:08:51 +0530

    Agri Plants Add Frontend

--- Agricuture_FrontEnd/src/App.js
+++ Agricuture_FrontEnd/src/App.js
@@ -1,8 +1,10 @@
+import Header from './components/Header';
 import AddAgriEquipment from './components/Equipment/AddAgriEquipment';
 import AgriEquipment from './components/Equipment/AllEquipment';
 import AddAgriSeed from './components/Seed/AddAgriSeed';
 import AgriSeed from './components/Seed/AllSeed';
-import Header from './components/Header';
+import AddAgriPlant from './components/Plant/AddAgriPlant';
+import AgriPlant from './components/Plant/AllPlant';
 
 import {BrowserRouter as Router, Route} from "react-router-dom"
 
@@ -16,6 +18,8 @@ function App() {
       <Route path="/agriequipment/all" exact component = {AgriEquipment}></Route>
       <Route path="/agriseed/add" exact component = {AddAgriSeed}></Route>
       <Route path="/agriseed/all" exact component = {AgriSeed}></Route>
+      <Route path="/agriplant/add" exact component = {AddAgriPlant}></Route>
+      <Route path="/agriplant/all" exact component = {AgriPlant}></Route>
 
     </div>
     </Router>
