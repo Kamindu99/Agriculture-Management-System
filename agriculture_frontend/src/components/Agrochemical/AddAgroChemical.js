b3025e1c1b36f003e256426c7b59b5da6da2d342
kamindu.g <kamindu.g@informaticsint.com>
2023-06-26 15:26:29 +0530

    agro chemical frontend

--- agriculture_frontend/src/components/Agrochemical/AddAgroChemical.js
+++ agriculture_frontend/src/components/Agrochemical/AddAgroChemical.js
@@ -1,132 +1,127 @@
-import React,{useState} from "react"
-import {useHistory} from 'react-router-dom';
+import React, { useState } from "react"
+import { useHistory } from 'react-router-dom';
 import axios from "axios";
 import { Form } from "react-bootstrap";
 
-const AddAgroChemical = ()=>{
+const AddAgroChemical = () => {
 
-     const [validated, setValidated] = useState(false);
+  const [validated, setValidated] = useState(false);
 
-     let history = useHistory();
+  let history = useHistory();
 
-     const[agrochemicalName,setName]=useState("");
-     const[description,setDescription]=useState("");
-     const[price,setPrice]=useState("");
-     const[,setMessage]=useState("");
-     const[agrochemicalImage,setFileName]=useState("");
-   
-     const onChangeFile= e=>{
-         setFileName(e.target.files[0]);
-     }
-   
-   const changeOnClick =(e)=>{
+  const [agrochemicalName, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [, setMessage] = useState("");
+  const [agrochemicalImage, setAgrochemicalImage] = useState("");
+
+  const changeOnClick = (e) => {
     const form = e.currentTarget;
     if (form.checkValidity() === false) {
       e.preventDefault();
       e.stopPropagation();
     }
 
-else{
-       e.preventDefault();
-   
-       const formData=new FormData();
-       formData.append("agrochemicalName",agrochemicalName);
-       formData.append("description",description);
-       formData.append("price",price);
-       formData.append("agrochemicalImage",agrochemicalImage);
-   
-       setName("");
-       setDescription("");
-       setPrice("");
-       
-   
-       axios
-       .post("https://agribackend.onrender.com/agrochemical/admin/add",formData)
-       .then(
-        (res)=>setMessage(res.data))
-        
-       .catch((err)=>{
-           console.log(err);
-       });
-       history.push("/agrochemical/all");
-       alert("New Agrochemical Added Successful")
-      }
-      setValidated(true);
-   };
-    return (
-      <div className="pagemargin">
+    else {
+      e.preventDefault();
+
+      let agrochemical = {
+        agrochemicalName,
+        description,
+        price,
+        agrochemicalImage
+      };
+
+      axios
+        .post("https://agribackend.onrender.com/agrochemical/admin/add", agrochemical)
+        .then(
+          (res) => setMessage(res.data))
+
+        .catch((err) => {
+          console.log(err);
+        });
+      history.push("/agrochemical/all");
+      alert("New Agrochemical Added Successful")
+    }
+    setValidated(true);
+  };
+  return (
+    <div className="pagemargin">
       <div>
-      <br/>
+        <br />
         <div className="container">
-        <div className="w-75 mx-auto shadow p-5">
-          <h2 className="text-center mb-4">Add Agrochemical</h2>
+          <div className="w-75 mx-auto shadow p-5">
+            <h2 className="text-center mb-4">Add Agrochemical</h2>
 
+            <Form noValidate validated={validated} class="signup-form" onSubmit={changeOnClick} encType="multipart/form-data">
+              <div className="form-group" controlId="validationCustom01">
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="Enter Name"
+                  name="agrochemicalName"
+                  value={agrochemicalName}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid name.
+                </Form.Control.Feedback>
+              </div>
+              <div className="form-group">
+                <textarea
+                  type="text"
+                  rows="6"
+                  className="form-control form-control-lg"
+                  placeholder="Enter Description"
+                  name="description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide Description
+                </Form.Control.Feedback>
+              </div>
+              <div className="form-group">
+                <input
+                  type="number"
+                  className="form-control form-control-lg"
+                  placeholder="Enter Price"
+                  name="price"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide Price
+                </Form.Control.Feedback>
+              </div>
 
+              <lable class="label-title"><b>Add an Image*</b>
+                <div class="mb-3">
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="Enter Agro Chemical Image URL"
+                  name="agrochemicalImage"
+                  value={agrochemicalImage}
+                  onChange={(e) => setAgrochemicalImage(e.target.value)}
+                  required
+                />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide Image.
+                  </Form.Control.Feedback>
+                </div></lable>
 
-
-
-          
-          <Form noValidate validated={validated} class="signup-form" onSubmit={changeOnClick} encType="multipart/form-data">
-            <div className="form-group" controlId="validationCustom01">
-              <input
-                type="text"
-                className="form-control form-control-lg"
-                placeholder="Enter Name"
-                name="agrochemicalName"
-                value={agrochemicalName}
-                onChange={(e)=>setName(e.target.value)}
-                required
-              />
-               <Form.Control.Feedback type="invalid">
-              Please provide a valid name. 
-            </Form.Control.Feedback>
-            </div>
-            <div className="form-group">
-              <textarea
-                type="text"
-                rows="6"
-                className="form-control form-control-lg"
-                placeholder="Enter Description"
-                name="description"
-                value={description}
-                onChange={(e)=>setDescription(e.target.value)}
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-              Please provide Description
-            </Form.Control.Feedback>
-            </div>
-            <div className="form-group">
-              <input
-                type="number"
-                className="form-control form-control-lg"
-                placeholder="Enter Price"
-                name="price"
-                value={price}
-                onChange={(e)=>setPrice(e.target.value)}
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-              Please provide Price 
-            </Form.Control.Feedback>
-            </div>
-
-            <lable class="label-title"><b>Add an Image*</b>
-            <div class="mb-3">
-            <input class="form-control" required type="file" id="formFile" filename="agrochemicalImage" onChange={onChangeFile}/>
-            <Form.Control.Feedback type="invalid">
-              Please provide Image. 
-            </Form.Control.Feedback>
-            </div></lable>
-
-            <button className="btn btn-primary btn-block">Done</button>
-          </Form>
+              <button className="btn btn-primary btn-block">Done</button>
+            </Form>
+          </div>
         </div>
       </div>
-      </div>
-      <br/>
-      </div>
-    );
+      <br />
+    </div>
+  );
 };
 
 export default AddAgroChemical;
--- agriculture_frontend/src/components/Agrochemical/AllAgroChemical.js
+++ agriculture_frontend/src/components/Agrochemical/AllAgroChemical.js
@@ -6,133 +6,186 @@ import Header from '../Header';
 import Footer from '../Footer';
 
 export default class AgroChemical extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          agrochemicals: [],
-        };
-      }
-    
-      componentDidMount() {
-        this.retrieveagrochemical();
-      }
-    
-      retrieveagrochemical() {
-        axios.get("https://agribackend.onrender.com/agrochemical").then((res) => {
-          if (res.data.success) {
-            this.setState({
-              agrochemicals: res.data.existingAgrochemical,
-            });
-          }
-        });
-      }
-    
-    
-      filterData(agrochemicals, searchkey) {
-        const result = agrochemicals.filter(
-          (post) =>
-            post.agrochemicalName.toLowerCase().includes(searchkey) ||
-            post.agrochemicalName.toUpperCase().includes(searchkey)
-        );
-        this.setState({ agrochemicals: result });
-      }
-    
-      handleSearchArea = (e) => {
-        const searchkey = e.currentTarget.value;
-    
-        axios.get("https://agribackend.onrender.com/agrochemical").then((res) => {
-          if (res.data.success) {
-            this.filterData(res.data.existingAgrochemical, searchkey);
-          }
-        });
-      };
-    
-      render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      agrochemicals: [],
+      isLoading: false
+    };
+  }
+
+  componentDidMount() {
+    this.retrieveagrochemical();
+  }
+
+  retrieveagrochemical() {
+    this.setState({ isLoading: true });
+
+    axios.get("https://agribackend.onrender.com/agrochemical")
+      .then((res) => {
+        if (res.data.success) {
+          this.setState({
+            agrochemicals: res.data.existingAgrochemical,
+            isLoading: false,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({ isLoading: false });
+      });
+  }
 
 
-    return(
+  filterData(agrochemicals, searchkey) {
+    const result = agrochemicals.filter(
+      (post) =>
+        post.agrochemicalName.toLowerCase().includes(searchkey) ||
+        post.agrochemicalName.toUpperCase().includes(searchkey)
+    );
+    this.setState({ agrochemicals: result });
+  }
+
+  handleSearchArea = (e) => {
+    const searchkey = e.currentTarget.value;
+    this.setState({ isLoading: true });
+    axios.get("https://agribackend.onrender.com/agrochemical").then((res) => {
+      if (res.data.success) {
+        this.filterData(res.data.existingAgrochemical, searchkey);
+        this.setState({ isLoading: false });
+      }
+    })
+      .catch((error) => {
+        console.log(error);
+        this.setState({ isLoading: false });
+      }
+      );
+  };
+
+  render() {
+
+
+    return (
       <div>
-        <Header/>
-        <div className="pagemargin" style={{background:"#ccccff"}}>
+        <Header />
+        <div className="pagemargin">
 
 
-<div     class=""
-              style={{
-                
-                backgroundColor: "hsla(101, 27%, 53%, 0.27)",
-                paddingBottom: "5px",
-                paddingTop: "7px",
-                
-              }}
+          <div class=""
+            style={{
+
+              backgroundColor: "hsla(101, 27%, 53%, 0.27)",
+              paddingBottom: "5px",
+              paddingTop: "7px",
+              display: 'none'
+            }}
+          >
+            <button
+              style={{ marginLeft: "30px" }}
+              id="search-button"
+              type="button"
+              class="btn btn-primary"
             >
+              <i class="fas fa-search"></i><a style={{ textDecoration: "none", color: "white" }} href="/agrochemical/add">Add New Agrochemical</a>
+            </button>
+            <div style={{ float: "right", marginRight: "20px" }}>
+              <input
+                id="search-input"
+                type="search"
+                style={{ width: "400px" }}
+                placeholder="Search Agrochemical"
+                onChange={this.handleSearchArea}
+              />
+
               <button
-              style={{marginLeft:"30px"}}
-                    id="search-button"
-                    type="button"
-                    class="btn btn-primary"
-                  >
-                    <i class="fas fa-search"></i><a style={{textDecoration:"none",color:"white"}} href="/agrochemical/add">Add New Agrochemical</a>
-                  </button>
-           <div  style={{float:"right",marginRight:"20px"}}>
-                    <input
-                      id="search-input"
-                      type="search"
-                      style={{width:"400px"}}
-                      placeholder="Search Agrochemical"
-                      onChange={this.handleSearchArea}
-                    />
-                 
-                  <button
-                    id="search-button"
-                    type="button"
-                    class="btn btn-primary"
-                  >
-                    <i class="fas fa-search"></i>
-                  </button>
-                  </div>
+                id="search-button"
+                type="button"
+                class="btn btn-primary"
+              >
+                <i class="fas fa-search"></i>
+              </button>
             </div>
-           
-
-
-            
-           <br/> <br/>
- 
-
-      <div class="container">
-      <Row xs={1} md={3} className="g-4" id="by" class="rounded">
-                {this.state.agrochemicals.map((eq, idx) => (
-    <div class="pt-1 ">
-      <div class="col-md-6 col-lg-12 pb-3">
-
-        <div class="card card-custom bg-white border-white border-0">
-          <div class="card-custom-img" ><img style={{height:"200px",width:"100%"}} src={`${eq.agrochemicalImage}`}/></div>
-          <div class="card-custom-avatar">
-            <img class="img-fluid" src={`${eq.agrochemicalImage}`}alt="Avatar" />
           </div>
-          <div class="card-body" style={{overflowY: "auto"}}>
-            <h4 class="card-title">{idx + 1}. &nbsp;{eq.agrochemicalName}</h4>
-            <p class="card-text">Card has minimum height set but will <a href="https://getbootstrap.com/docs/4.0/components/card/#card-decks" target="_blank">card-decks</a> to align multiple cards nicely in a row.</p>
+
+
+
+          <div class="jumbotron ">
+            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+              <h1 className="seedshomecsscard">
+                <img style={{ width: '50px', height: '50px' }} src={`https://png.pngtree.com/png-clipart/20230105/ourmid/pngtree-illustration-of-hands-holding-plant-seeds-in-the-context-of-one-png-image_6552749.png`} /> කෘමිනාශක / වල් නාශක
+              </h1>
+              <div className='searchbarnew' style={{ display: "flex", alignItems: "center", marginTop: '-4%' }}>
+                <div class="form">
+                  <i class="fa fa-search"></i>
+                  <input type="text" class="form-control form-input" placeholder="Search Agro Chemicals..." onChange={this.handleSearchArea} />
+                  <span class="left-pan"><i class="fa fa-microphone"></i></span>
+                </div>
+
+              </div>
+            </div>
+
+            <p style={{ marginBottom: '-20px' }}>
+              පලිබෝධනාශක යනු කෘමීන් විසින් සිදු කරන හානිය වැළැක්වීමට, පාලනය කිරීමට හෝ අවම කිරීමට යොදා ගන්නා සංයෝගයක් හෝ සංයෝග මිශ්‍රණයකි.
+              පලිබෝධනාශකයක් රසායනික සංයෝගයක්, ජීව විද්‍යාත්මක ප්‍රතිකාරකයෙක් (වෛරස් හෝ බැක්ටීරියා වැනි) , ප්‍රතිමයික්‍රෝබීය ,
+              විශබීජ නාශකයක් හෝ කෘමීන්ට එරෙහිව යොදා ගන්නා උපකරණයක් විය හැකිය. පලිබෝධකයන්ට ‍මිනිසා සමඟ ආහාර සඳහා තරඟ කරන,
+              දේපල විනාශ කරන ව්‍යසනයක් වන හෝ පතුර වන හෝ නොසන්සුන්තාවක් ඇති කරන කෘමීන්, ශාක රෝග කාරක බීජය, වල් පැළ.
+            </p>
           </div>
-          <div class="card-footer" style={{background: "inherit; border-color: inherit;"}}>
-            <a href="#" class="btn btn-primary">Option</a>&nbsp;&nbsp;&nbsp;&nbsp;
-            <a href="#" class="btn btn-outline-primary">Other option</a>
+
+
+
+          <div class="container">
+            {this.state.isLoading ? (
+              <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "40vh" }}>
+                <div className="spinner-border text-success" style={{ width: "100px", height: "100px", animationDuration: "1.5s" }} role="status"></div>
+              </div>
+            ) :
+              <>
+                {this.state.agrochemicals.length == 0 ?
+                  <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "40vh" }}>
+                    <h2>No Agro Chemicals Found</h2>
+                  </div> :
+                  <>
+                    <Row xs={1} md={3} className="g-4" id="by" class="rounded">
+
+
+                      {this.state.agrochemicals.map((eq, idx) => (
+                        <div class="pt-1 ">
+                          <div class="col-md-6 col-lg-12 pb-3">
+
+                            <div class="card card-custom bg-white border-white border-0">
+                              <div class="card-custom-img" ><img style={{ height: "200px", width: "100%" }} src={`${eq.agrochemicalImage}`} /></div>
+                              <div class="card-custom-avatar">
+                                <img class="img-fluid" src={`${eq.agrochemicalImage}`} alt="Avatar" />
+                              </div>
+                              <div class="card-body" style={{ overflowY: "auto" }}>
+                                <h4 class="card-title">{idx + 1}. &nbsp;{eq.agrochemicalName}</h4>
+                                <p class="card-text">Card has minimum height set but will <a href="https://getbootstrap.com/docs/4.0/components/card/#card-decks" target="_blank">card-decks</a> to align multiple cards nicely in a row.</p>
+                              </div>
+                              <div class="card-footer" style={{ background: "inherit; border-color: inherit;" }}>
+                                <a href="#" class="btn btn-primary">Option</a>&nbsp;&nbsp;&nbsp;&nbsp;
+                                <a href="#" class="btn btn-outline-primary">Other option</a>
+                              </div>
+                            </div>
+
+                          </div>
+
+
+                        </div>
+                      ))}
+
+                    </Row>
+                  </>
+                }
+              </>}
           </div>
+
+          <br /><br />
         </div>
-
+        <Footer />
       </div>
-   
-  
-    </div>
-    ))}
-    </Row>
-  </div>
-
-<br/><br/>
-</div>
-<Footer/>
-        </div>
-   );
-}
+    );
+  }
 }
 
 
